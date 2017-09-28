5d3ef6463eb56b999260c3368531bccc49c53051
Bryan <bryanporras2033@gmail.com>
2017-09-28 00:29:30 -0400

    got inquirer prompt working

--- ClozeCard.js
+++ ClozeCard.js
@@ -2,22 +2,61 @@ var ClozeCard = require("./library/cloze.js");
 var cardDate = require('./json/cloze.json');
 var inquirer = require('inquirer');
 
-startgame();
 
-function startgame(){
-    // ?
-}
+var count = 0
+
+// function startgame(){
+
+
+   var askQuestion = function(){
+
+        if(count < cardDate.length){
+            console.log("NEW QUESTION");
+
+            inquirer.prompt([
+                {
+                    name: "answer",
+                    message: cardDate[count].partial
+                }
+            ]).then(function(result){
+
+                if(result.answer === cardDate[count].cloze){
+                    console.log("you got it right" + "\n" + cardDate[count].text)
+                    count ++;
+                    askQuestion();
+                } else {
+                    console.log("you got it wrong" + "\n" + cardDate[count].text)
+                    count ++
+                    askQuestion();
+                }
+            })
+        }
+   }
+// }
+
+// startgame();
+askQuestion();
+
+
+
+
+
+
+
+
+
+
+
+
 
 function endGame(){
     //if count reached limit?
 }
 
 function round(){
-    //count?
+    var count = 0;
 }
 
 function prompts(){
     //inquirer?
 }
-
-console.log(cardDate[1].text);
